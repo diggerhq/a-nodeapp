a1fa2289e141661d3d82a22fc3fb587c58bff4b0
Igor Zalutski <izalutski@gmail.com>
2020-11-19 13:33:21 +0000

    Add testvar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const testvar = process.env.TESTVAR;
+  res.render('index', { title: 'Testvar: ' + testvar });
 });
 
 module.exports = router;
