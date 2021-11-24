1716d998c06ca54a406542fe519e2c6cc295da55
motatoes <moe.habib9@gmail.com>
2021-11-24 21:26:30 +0300

    include status code

--- lambda.js
+++ lambda.js
@@ -1,5 +1,8 @@
 
 exports.handler =  async function(event, context) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  return "<h1>Hello from digger!!</h1>"
+  return {
+    statusCode: 200,
+    body: "<h1>Hello from digger!!</h1>"
+  }
 }
