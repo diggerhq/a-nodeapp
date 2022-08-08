6e99b2b1fe4d9b3973244a25220302972299a62e
motatoes <moe.habib9@gmail.com>
2022-08-08 13:02:51 +0100

    mysql database connection

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 // import { createClient } from 'redis';
 var redis = require("redis");
+let mysql = require('mysql');
 
 
 async function redis_test(url) {
@@ -10,19 +11,27 @@ async function redis_test(url) {
   });
 
   client.on('error', (err) => console.log('Redis Client Error', err));
-
   await client.connect();
-
   await client.set('key', 'value');
   const value = await client.get('key');
   return value
 }
 
+async function mysql_test(url) {
+  let connection = mysql.createConnection({
+    url: url
+  });
+  await connection.connect()
+
+}
 /* GET home page. */
 router.all('*', async function(req, res, next) {
   
   const REDIS_URL = process.env.REDIS_URL
+  const DATABASE_URL = process.env.DATABASE_URL
   let value = await redis_test(REDIS_URL)
+  await mysql_test(DATABASE_URL)
+  
   const testvar = process.env.TESTVAR + "reds_value: " + value;
 
   const host = req.get('host')
